f1908308fcd22c7c9efe365faf56a3ecfa6ee221
JeffreyMJohnson <jefjohms@gmail.com>
2015-09-11 14:46:43 -0700

    have startup working.

---
+++ include/GLFramework.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "gl_core_4_4.h"
+#include "GLFW\glfw3.h"
+#include "glm\vec4.hpp"
+#include "FlyCamera.h"
+
+#include <iostream>
+
+
+using glm::vec4;
+
+struct Color
+{
+	float r = 0, g = 0, b = 0, a = 0;
+	Color() {};
+	Color(float r, float g, float b, float a)
+	{
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = a;
+	}
+};
+
+
+struct Window
+{
+	int height = 0;
+	int width = 0;
+	char* title = "";
+	GLFWwindow* handle = nullptr;
+	Color clearColor;
+};
+
+
+class GLFramework
+{
+public:
+	static bool Startup(int height, int width, char* title, Color clearColor);
+	static Color GetClearColor();
+	static void SetClearColor(Color color);
+	static void Cleanup();
+
+private:
+	static Window* sWindow;
+	static Camera* sCamera;
+};
---
+++ source/GLFramework.cpp
@@ -0,0 +1,61 @@
+#include "..\include\GLFramework.h"
+
+Window* GLFramework::sWindow = new Window();
+Camera* GLFramework::sCamera = new FlyCamera();
+
+bool GLFramework::Startup(int height, int width, char * title, Color clearColor)
+{
+	if (!glfwInit())
+	{
+		return false;
+	}
+	sWindow->height = height;
+	sWindow->width = width;
+	sWindow->title = title;
+
+	sWindow->handle = glfwCreateWindow(height, width, title, nullptr, nullptr);
+
+	if (nullptr == sWindow->handle)
+	{
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(sWindow->handle);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(sWindow->handle);
+		glfwTerminate();
+		return false;
+	}
+
+	SetClearColor(clearColor);
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+}
+
+Color GLFramework::GetClearColor()
+{
+	Color result;
+	if (nullptr != sWindow)
+	{
+		result = sWindow->clearColor;
+	}
+	return result;
+}
+
+void GLFramework::SetClearColor(Color color)
+{
+	if (nullptr != sWindow)
+	{
+		sWindow->clearColor = color;
+		glClearColor(sWindow->clearColor.r, sWindow->clearColor.g, sWindow->clearColor.b, sWindow->clearColor.a);
+	}
+}
+
+void GLFramework::Cleanup()
+{
+	delete sWindow;
+}
