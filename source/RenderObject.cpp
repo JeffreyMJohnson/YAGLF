5c886f6489cea7aadf3bdf3d82a6c2e03b596988
Jeffrey Johnson <jefjohms@gmail.com>
2015-09-27 12:38:06 -0700

    refactored render object to own it's own buffer load, buffer delete, and draw call.

--- source/RenderObject.cpp
+++ source/RenderObject.cpp
@@ -15,13 +15,27 @@ void RenderObject::LoadBuffers(Geometry & geometry)
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, geometry.indices.size() * sizeof(uint), geometry.indices.data(), GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);//position
-	glEnableVertexAttribArray(1);//color
+	glEnableVertexAttribArray(1);//color in shader right now.
+	glEnableVertexAttribArray(2);//normal
+	glEnableVertexAttribArray(3);//tangent
+	glEnableVertexAttribArray(4);//UV coord
+
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec4) * 1));
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_TRUE, sizeof(Vertex), (void*)(sizeof(glm::vec4) * 2));
+	glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec4) * 3));
+	glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec4) * 4));
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void RenderObject::Draw()
+{
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 void RenderObject::DeleteBuffers()
